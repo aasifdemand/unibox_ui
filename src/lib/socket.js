9ec4d23363c9b1c612d3a6901facac85f2b6e74d
aasifdemand <aasifdemand@gmail.com>
2026-03-12 00:06:28 +0530

    feat: fixed editor padding and socket polling

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -11,6 +11,7 @@ const SOCKET_URL = import.meta.env.VITE_API_BASE_URL || defaultBaseUrl;
 export const socket = io(SOCKET_URL, {
     autoConnect: false, // We'll connect manually when we confirm auth
     withCredentials: true,
+    transports: ['websocket', 'polling'],
 });
 
 export const connectSocket = () => {
