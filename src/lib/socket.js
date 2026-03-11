bf41663f2ad1a29f169b57686de5e34581c92aea
aasifdemand <aasifdemand@gmail.com>
2026-03-12 00:28:58 +0530

    feat: new socket polling

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,16 +1,13 @@
 import { io } from 'socket.io-client';
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1';
-const defaultBaseUrl = API_URL.replace(/\/api\/v1\/?$/, '') || 'http://localhost:8080';
-
-const SOCKET_URL = import.meta.env.VITE_API_BASE_URL || defaultBaseUrl;
+const SOCKET_BASE = API_URL.replace(/\/api\/v1\/?$/, '');
 
 // Initialize a generic socket client
-// Make sure this doesn't auto-connect initially if we don't have a token,
-// though in a dashboard app we usually have cookies available.
-export const socket = io(SOCKET_URL, {
+export const socket = io(SOCKET_BASE, {
     autoConnect: false, // We'll connect manually when we confirm auth
     withCredentials: true,
+    transports: ['websocket', 'polling'],
 });
 
 export const connectSocket = () => {
